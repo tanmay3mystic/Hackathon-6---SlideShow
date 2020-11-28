c03d101c54cc290e2d65a94bd9a0f14e46266104
Tanmay Rathod <tanmay.3mystic@gmail.com>
2020-11-28 14:00:39 +0530

    hrst

--- src/components/App.js
+++ src/components/App.js
@@ -14,28 +14,31 @@ import '../styles/App.css';
 const App = ({slides}) => {
   const [index , setIndex]=useState(0);
   const [currentSlide , setCurrentSlide]=useState(slides[index]);
-  
+ 
   return (
     <div className="App">
     <div  id="navigation">
     <button type="button" disabled={index==0} data-testid="button-restart" onClick={()=>{
          setIndex(0);
+         setCurrentSlide(slides[index]);
       }}>Restart</button>
 
      <button type="button" disabled={index==slides.length-1} data-testid="button-next" onClick={()=>{
         setIndex(index+1);
+        setCurrentSlide(slides[index]);
       }}>Next</button>
 
     <button type="button"  disabled={index==0} data-testid="button-prev" onClick={()=>{
         setIndex(index-1);
-      }}>previous</button>
+        setCurrentSlide(slides[index]);
+      }}>Prev</button>
  
     </div>
     {/* <slideShow sl={slides[0]}/> */}
     
     <div id="slide">
-      <h1 data-testid="title">{slides[index].title}</h1>
-      <p data-testid="text">{slides[index].text}</p>
+      <h1 data-testid="title">{currentSlide.title}</h1>
+      <p data-testid="text">{currentSlide.text}</p>
     </div>
     </div>
   )
